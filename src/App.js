bf491814946ed7b98e35ab0abd7b5ed5ecf6c065
InbarDanieli <inbar0308@gmail.com>
2023-02-02 23:43:06 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Dice from './Dice';
 
 function App() {
+  const [gameDice, setGameDice] = useState(resetGame())
+
+  function resetGame() {
+    let gameArr = []
+
+    for (let i = 0; i < 10; i++) {
+      gameArr.push({
+        value: Math.floor(Math.random() * 6) + 1,
+        chosen: false,
+        id: i + 1
+      })
+    }
+    return gameArr
+  }
+
+  function ChoosenRolls(id) {
+    setGameDice(gameDice.map((dice) => {
+      if (dice.id === id) {
+        return { ...dice, chosen: !dice.chosen }
+      }
+      return { ...dice }
+    }))
+  }
+
+  function rollDice() {
+    setGameDice(gameDice.map((dice) => {
+      if (!dice.chosen) {
+        return { ...dice, value: Math.floor(Math.random() * 6) + 1 }
+      }
+      return { ...dice }
+    }))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='game'>
+        <div className='instructions'>
+          <h1>Tenzies</h1>
+          <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+        </div>
+        <div className='diceContainer'>
+          {gameDice.map(({ id, chosen, value }) =>
+            <Dice
+              onChose={() => ChoosenRolls(id)}
+              chosen={chosen}
+              value={value}
+            />
+          )}
+        </div>
+        <button onClick={rollDice} className='roolBtn'>Roll</button>
+      </div>
     </div>
   );
 }
---
+++ src/Dice.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Dice(props) {
+
+  return (
+    <div onClick={props.onChose} className={`dice ${props.chosen && "choose"}`}>{props.value}</div>
+  )
+}
